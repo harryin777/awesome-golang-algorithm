ed86ed731b2a3b73d3b373a825d102a3f2f6c808
harryyyin <harryyyin@tencent.com>
2024-03-29 14:17:18 +0800

    3.29

--- leetcode/1901-2000/1928.Minimum-Cost-to-Reach-Destination-in-Time/Solution.go
+++ leetcode/1901-2000/1928.Minimum-Cost-to-Reach-Destination-in-Time/Solution.go
@@ -1,5 +1,39 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func minCost(maxTime int, edges [][]int, passingFees []int) int {
+	// dp 代表着i分钟内，从 0 到 j 点所需要的最小过路费
+	dp := make([][]int, maxTime+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(passingFees))
+		for j := 0; j < len(dp[i]); j++ {
+			dp[i][j] = 1000 * 1001
+		}
+
+	}
+	dp[0][0] = 0
+	for i := 1; i <= maxTime; i++ {
+		for _, edge := range edges {
+			x, y, fee := edge[0], edge[1], edge[2]
+			dp[i][y] = min(dp[i][y], dp[i-1][x]+fee)
+		}
+	}
+
+	res := 1000 * 1001
+	for _, val := range dp {
+		res = min(res, val[len(edges)-1])
+	}
+
+	if res == 1000*1001 {
+		return -1
+	}
+
+	return res
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+
+	return y
 }
--- leetcode/401-500/0435.Non-overlapping-Intervals/Solution.go
+++ leetcode/401-500/0435.Non-overlapping-Intervals/Solution.go
@@ -1,5 +1,55 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+// 超出时间限制
+func eraseOverlapIntervals(intervals [][]int) int {
+	if len(intervals) == 0 {
+		return 0
+	}
+
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][1] < intervals[j][1]
+	})
+	var slice []int
+	for index, interval := range intervals {
+		tmp := interval
+		value := 1
+		for j := index + 1; j < len(intervals); j++ {
+			if tmp[1] <= intervals[j][0] {
+				tmp = intervals[j]
+				value++
+			}
+		}
+		slice = append(slice, value)
+	}
+
+	sort.Ints(slice)
+	return len(slice) - slice[len(slice)-1]
+
+}
+
+func eraseOverlapIntervals2(intervals [][]int) int {
+	if len(intervals) == 0 {
+		return 0
+	}
+
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][1] < intervals[j][1]
+	})
+
+	dp := make([]int, len(intervals))
+	dp[0] = 1
+	preMaxRight := intervals[0][1]
+	for i := 1; i < len(intervals); i++ {
+		curOne := intervals[i]
+		if curOne[0] > preMaxRight {
+			dp[i] = dp[i-1] + 1
+			preMaxRight = curOne[1]
+		} else {
+			dp[i] = dp[i-1]
+		}
+	}
+
+	return dp[len(intervals)-1]
 }
