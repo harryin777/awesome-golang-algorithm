f9e53cbc27a2666844708247002c856e16e556e4
harryyyin <harryyyin@tencent.com>
2024-06-04 13:06:42 +0800

    6.4

--- leetcode/301-400/0337.House-Robber-III/Solution.go
+++ leetcode/301-400/0337.House-Robber-III/Solution.go
@@ -7,6 +7,26 @@ type TreeNode struct {
 }
 
 func rob(root *TreeNode) int {
+	robRoot, notRob := getAmt(root)
 
-	return 0
+	return max(robRoot, notRob)
+}
+
+func getAmt(root *TreeNode) (int, int) {
+	if root == nil {
+		return 0, 0
+	}
+	leftRob, leftNotRob := getAmt(root.Left)
+	rightRob, rightNotRob := getAmt(root.Right)
+	robRoot := root.Val + leftNotRob + rightNotRob
+	notRob := max(leftRob, leftNotRob) + max(rightRob, rightNotRob)
+	return robRoot, notRob
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
 }
