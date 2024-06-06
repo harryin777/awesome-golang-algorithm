b2bfb8b3d7c37748ed4e20026c66f11b59c9c5bf
harryyyin <harryyyin@tencent.com>
2024-06-06 09:49:14 +0800

    6.6

--- leetcode/301-400/0397.Integer-Replacement/Solution.go
+++ leetcode/301-400/0397.Integer-Replacement/Solution.go
@@ -3,3 +3,33 @@ package Solution
 func Solution(x bool) bool {
 	return x
 }
+
+func integerReplacement(n int) int {
+	dp := make([]int, n+2)
+	dp[0] = 1
+	dp[1] = 0
+	dp[2] = 1
+	for i := 3; i < len(dp); i++ {
+		if i%2 == 0 {
+			dp[i] = dp[i/2] + 1
+		} else {
+			dp[i] = dp[i-1] + 1
+		}
+	}
+
+	for i := 3; i <= n; i++ {
+		if i%2 != 0 {
+			dp[i] = min(dp[i], dp[i+1]+1)
+		}
+	}
+
+	return dp[n]
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+
+	return y
+}
--- leetcode/301-400/0397.Integer-Replacement/Solution_test.go
+++ leetcode/301-400/0397.Integer-Replacement/Solution_test.go
@@ -10,18 +10,18 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		//{"TestCase", 15, 5},
+		//{"TestCase", 1000, 12},
+		{"TestCase", 250, 10},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := integerReplacement(c.inputs)
 			if !reflect.DeepEqual(got, c.expect) {
 				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
 					c.expect, got, c.inputs)
@@ -30,10 +30,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
